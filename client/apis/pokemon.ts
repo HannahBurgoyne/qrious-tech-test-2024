d1a8a18c1c906322a3a9cdc2f9f6bf0d4ed8ae2a
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-09-23 18:30:12 +1200

    fetch generation 1 data

--- client/apis/pokemon.ts
+++ client/apis/pokemon.ts
@@ -1,1 +1,12 @@
 import request from 'superagent'
+import { PokemonGeneration } from '../../models/pokemon'
+
+export async function fetchPokemonByGeneration(
+  generation: number,
+): Promise<PokemonGeneration> {
+  const res = await request.get(
+    `https://pokeapi.co/api/v2/generation/${generation}`,
+  )
+
+  return res.body as PokemonGeneration
+}
