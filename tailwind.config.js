7932529d1fa213d84213cac612a1b5e6a7c3b432
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-09-24 10:09:26 +1200

    remove fonts temporarily

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,7 @@ export default {
   content: ['./index.html', './client/**/*.tsx'],
   theme: {
     extend: {
-      extend: {
-        fontFamily: {
-          heading: ['Averia Serif Libre', 'serif'],
-          body: ['Quicksand', 'sans-serif'],
-        },
-        fontSize: {
-          'heading-lg': '2.5rem',
-          'heading-md': '1.5rem',
-          'heading-sm': '1.2rem',
-          'body-xl': '2rem',
-          'body-lg': '1.125rem',
-          'body-md': '1rem',
-          'body-xsm': '0.5rem',
-        },
-        fontWeight: {
-          'heading-regular': 400,
-          'heading-bold': 700,
-          'body-light': 300,
-          'body-regular': 400,
-          'body-bold': 700,
-        },
-      },
+      extend: {},
     },
     plugins: [],
   },
