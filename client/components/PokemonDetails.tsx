955c5d45b7e4c2e31106b48a208d149c01b2c7f1
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-09-24 09:42:17 +1200

    move pokemon details into new component

---
+++ client/components/PokemonDetails.tsx
@@ -0,0 +1,47 @@
+import { Pokemon } from '../../models/pokemon'
+
+interface Props {
+  pokemon: Pokemon
+}
+
+function PokemonDetails({ pokemon }: Props) {
+  return (
+    <section>
+      <h1>{pokemon.name}</h1>
+      <p>
+        {' '}
+        Types:
+        {pokemon.types.map((type) => (
+          <p>
+            <a href={`${type.type.url}`}>{type.type.name}</a>
+          </p>
+        ))}
+      </p>
+      <img
+        src={`${pokemon.sprites.front_default}`}
+        alt={`sprite for ${pokemon.name}`}
+      />
+      <p>Height: {pokemon.height}</p>
+      <p>Weight: {pokemon.weight}</p>
+      <p>
+        {' '}
+        Abilities:
+        {pokemon.abilities.map((ability) => (
+          <p>
+            <a href={`${ability.ability.url}`}>{ability.ability.name}</a>
+          </p>
+        ))}
+      </p>
+      <p>
+        Moves:
+        {pokemon.moves.map((move) => (
+          <p>
+            <a href={`${move.move.url}`}>{move.move.name}</a>
+          </p>
+        ))}
+      </p>
+    </section>
+  )
+}
+
+export default PokemonDetails
--- client/components/PokemonList.tsx
+++ client/components/PokemonList.tsx
@@ -1,4 +1,5 @@
 import { usePokemon } from '../hooks/usePokemon'
+import PokemonDetails from './PokemonDetails'
 
 function PokemonList() {
   const { data, isError, isFetching } = usePokemon(1)
@@ -7,44 +8,7 @@ function PokemonList() {
 
   if (isError) return <p>Oops, error!</p>
 
-  if (data)
-    return data.map((pokemon) => (
-      <section>
-        <h1>{pokemon.name}</h1>
-        <p>
-          {' '}
-          Types:
-          {pokemon.types.map((type) => (
-            <p>
-              <a href={`${type.type.url}`}>{type.type.name}</a>
-            </p>
-          ))}
-        </p>
-        <img
-          src={`${pokemon.sprites.front_default}`}
-          alt={`sprite for ${pokemon.name}`}
-        />
-        <p>Height: {pokemon.height}</p>
-        <p>Weight: {pokemon.weight}</p>
-        <p>
-          {' '}
-          Abilities:
-          {pokemon.abilities.map((ability) => (
-            <p>
-              <a href={`${ability.ability.url}`}>{ability.ability.name}</a>
-            </p>
-          ))}
-        </p>
-        <p>
-          Moves:
-          {pokemon.moves.map((move) => (
-            <p>
-              <a href={`${move.move.url}`}>{move.move.name}</a>
-            </p>
-          ))}
-        </p>
-      </section>
-    ))
+  if (data) return data.map((pokemon) => <PokemonDetails pokemon={pokemon} />)
 }
 
 export default PokemonList
