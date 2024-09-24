9b9a02ce407865079fe9e24f8adc01643a15f598
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-09-24 15:00:40 +1200

    style component

--- client/components/PokemonDetails.tsx
+++ client/components/PokemonDetails.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
 import { usePokemon } from '../hooks/usePokemon'
+import Icon from './UI/Icon'
 
 function PokemonDetails() {
   const { id } = useParams()
@@ -12,31 +13,49 @@ function PokemonDetails() {
   if (!pokemon) return <p>Pokémon not found!</p>
   return (
     <section className="bg-background p-10">
-      <div className="bg-white rounded p-5 max-w-3xl mx-auto grid grid-cols-2">
-        <div>
-          <h1 className="font-heading text-xl font-semibold">{pokemon.name}</h1>
-          <img
-            src={`${pokemon.sprites.front_default}`}
-            alt={`sprite for ${pokemon.name}`}
-            className="w-48"
-          />
-          <audio controls>
-            <source src={pokemon.cries.latest} type="audio/ogg" />
-            Your browser does not support the audio element.
-          </audio>
-          <p>Base experience: {pokemon.base_experience}</p>
-        </div>
-        <div>
-          <h2 className="font-heading text-lg font-semibold">Abilities:</h2>
-          {pokemon.abilities.map((ability) => (
-            <p>
-              <a href={`${ability.ability.url}`}>{ability.ability.name}</a>
-            </p>
-          ))}
+      <div className="bg-white rounded p-5 max-w-3xl mx-auto">
+        <div className="flex justify-between">
+          <div>
+            <h1 className="font-heading text-xl font-semibold">
+              {pokemon.name}
+            </h1>
+            <img
+              src={`${pokemon.sprites.front_default}`}
+              alt={`sprite for ${pokemon.name}`}
+              className="w-48"
+            />
+            <audio controls>
+              <source src={pokemon.cries.latest} type="audio/ogg" />
+              Your browser does not support the audio element.
+            </audio>
+          </div>
+          <div>
+            <div className="flex justify-start">
+              {pokemon.types.map((type) => (
+                <Icon backgroundColor={type.type.name}>
+                  <p className="text-sm font-body">
+                    <a href={`${type.type.url}`}>{type.type.name}</a>
+                  </p>
+                </Icon>
+              ))}
+            </div>
+            <h2 className="font-heading text-lg font-semibold">
+              Base experience:
+            </h2>
+            <p className="p-2">{pokemon.base_experience}</p>
 
-          <h2>Moves:</h2>
+            <h2 className="font-heading text-lg font-semibold">Abilities:</h2>
+            {pokemon.abilities.map((ability) => (
+              <p className="p-2" key={ability.ability.name}>
+                <a href={`${ability.ability.url}`}>{ability.ability.name}</a>
+              </p>
+            ))}
+          </div>
+        </div>
+        <h2 className="font-heading text-lg font-semibold mt-6">Moves:</h2>
+        <div className="grid grid-cols-5 gap-2">
           {pokemon.moves.map((move) => (
-            <p>
+            <p key={move.move.name}>
               <a href={`${move.move.url}`}>{move.move.name}</a>
             </p>
           ))}
