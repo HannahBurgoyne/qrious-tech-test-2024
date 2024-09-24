ded53d81bcc2874c8a1bb52d53de222f537d4371
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-09-25 10:07:32 +1200

    move test data to centralised location

--- client/components/PokemonList.test.tsx
+++ client/components/PokemonList.test.tsx
@@ -3,99 +3,11 @@ import { setupApp } from '../test-utils'
 import { beforeAll, describe, it, expect } from 'vitest'
 import nock from 'nock'
 import { waitFor } from '@testing-library/react'
+import { mockPokemonData } from '../test-utils'
 
 beforeAll(() => {
   nock.disableNetConnect()
 })
-
-const mockPokemonData = [
-  {
-    id: 1,
-    name: 'bulbasaur',
-    height: 7,
-    weight: 69,
-    sprites: {
-      front_default: 'test-front-bulbasaur.png',
-      back_default: 'test-back-bulbasaur.png',
-    },
-    base_experience: 64,
-    cries: {
-      latest: 'test-latest-cry-bulbasaur.ogg',
-      legacy: 'test-legacy-cry-bulbasaur.ogg',
-    },
-    abilities: [
-      {
-        ability: { name: 'Overgrow', url: 'test-ability' },
-        is_hidden: false,
-        slot: 1,
-      },
-      {
-        ability: { name: 'Chlorophyll', url: 'test-ability' },
-        is_hidden: true,
-        slot: 3,
-      },
-    ],
-    moves: [
-      {
-        move: { name: 'Tackle', url: 'test-move' },
-      },
-      {
-        move: { name: 'Vine Whip', url: 'test-move' },
-      },
-    ],
-    types: [
-      {
-        slot: 1,
-        type: { name: 'grass', url: 'test-type' },
-      },
-      {
-        slot: 2,
-        type: { name: 'poison', url: 'test-type' },
-      },
-    ],
-  },
-  {
-    id: 4,
-    name: 'charmander',
-    height: 6,
-    weight: 85,
-    sprites: {
-      front_default: 'test-front-charmander.png',
-      back_default: 'test-back-charmander.png',
-    },
-    base_experience: 62,
-    cries: {
-      latest: 'test-latest-cry-charmander.ogg',
-      legacy: 'test-legacy-cry-charmander.ogg',
-    },
-    abilities: [
-      {
-        ability: { name: 'Blaze', url: 'test-ability' },
-        is_hidden: false,
-        slot: 1,
-      },
-      {
-        ability: { name: 'Solar Power', url: 'test-ability' },
-        is_hidden: true,
-        slot: 3,
-      },
-    ],
-    moves: [
-      {
-        move: { name: 'Scratch', url: 'test-move' },
-      },
-      {
-        move: { name: 'Ember', url: 'test-move' },
-      },
-    ],
-    types: [
-      {
-        slot: 1,
-        type: { name: 'fire', url: 'test-type' },
-      },
-    ],
-  },
-]
 
 describe('PokemonList', () => {
   it('shows a list of pokemon', async () => {
