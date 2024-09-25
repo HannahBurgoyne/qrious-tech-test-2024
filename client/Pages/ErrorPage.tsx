8d0ab2ba1a993b07d06fdec54018215bd38ed9b7
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-09-25 15:22:34 +1200

    finish styling error page

--- client/Pages/ErrorPage.tsx
+++ client/Pages/ErrorPage.tsx
@@ -2,18 +2,20 @@ import { Link } from 'react-router-dom'
 
 function ErrorPage() {
   return (
-    <section className="p-20 flex flex-col justify-center items-center">
-      <h1 className="font-heading text-2xl p-2 font-semibold">
-        Oh no! That page cannot be found.
-      </h1>
-      <img
-        src="./images/crying-squirtle.gif"
-        alt="a gif of squirtle on his back crying"
-      />
-      <Link className="bg-fighting text-white rounded p-4 m-4" to={'/'}>
-        Go home
-      </Link>
-    </section>
+    <main className="bg-background min-h-screen p-5 flex flex-col justify-center items-center">
+      <section className="bg-white rounded p-10 flex flex-col justify-center items-center">
+        <h1 className="font-heading text-2xl p-2 font-semibold">
+          Oh no! That page cannot be found.
+        </h1>
+        <img
+          src="./images/crying-squirtle.gif"
+          alt="a gif of squirtle on his back crying"
+        />
+        <Link className="bg-fighting text-white rounded p-4 m-4" to={'/'}>
+          Go home
+        </Link>
+      </section>
+    </main>
   )
 }
 
